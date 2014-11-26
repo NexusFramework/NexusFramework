047f60e11788a9197b077af3a0d8579f41448ae1
ktaeyln <ktaeyln@gmail.com>
2014-11-25 22:44:38 -0500

    Fixed

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,7 @@
 var path = require('path')
 var assert = require('assert')
 
-describe('typeinclude', function() {
+describe('NexusFrameworkJS', function() {
 	var nexusframework;
 	it('index.js', function(){
 		nexusframework = require(path.dirname(__dirname) + path.sep + "index");
